00039b2076faf3655a266c738cb193508fc32b3e
David Fiorin <davidfiorin.gd@gmail.com>
2017-06-19 10:22:56 +0200

    - Loaded PersonalAR and MidnightPooh in RPP (RandomPersonalProject)

---
+++ PersonalAR/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit raycastHit;
+    private TouchableObject currentTouchableObject;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out raycastHit, Mathf.Infinity))
+            {
+                TouchableObject touchableObj = raycastHit.collider.gameObject.GetComponent<TouchableObject>();
+                if (touchableObj)
+                {
+                    //Set off the hitted object
+                    //currentTouchableObject.gameObject.SetActive(false);
+
+                    currentTouchableObject = touchableObj;
+
+                    Debug.Log(touchableObj.name);
+                    Debug.Log(Vector3.Distance(this.gameObject.transform.position, currentTouchableObject.transform.position));
+                }
+                else
+                {
+                    if (currentTouchableObject)
+                        currentTouchableObject = null;
+                }
+
+                Debug.Log(Vector3.Distance(this.gameObject.transform.position, currentTouchableObject.transform.position));
+            }
+        }
+    }
+}
